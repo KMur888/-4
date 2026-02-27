b87960ad085ef93a516205f08b76b7ba9a20e2a8
Kris <Fritthof@mail.ru>
2026-02-27 20:48:15 +0300

    Add files via upload

---
+++ Практическая4_4.4.py
@@ -0,0 +1,17 @@
+color1 = input("Введите первый цвет (красный, синий, желтый): ").lower()
+color2 = input("Введите второй цвет (красный, синий, желтый): ").lower()
+
+if (color1 == "красный" or color1 == "синий" or color1 == "желтый") and \
+   (color2 == "красный" or color2 == "синий" or color2 == "желтый"):
+    
+    if (color1 == "красный" and color2 == "синий") or (color1 == "синий" and color2 == "красный"):
+        print("фиолетовый")
+    elif (color1 == "красный" and color2 == "желтый") or (color1 == "желтый" and color2 == "красный"):
+        print("оранжевый")
+    elif (color1 == "синий" and color2 == "желтый") or (color1 == "желтый" and color2 == "синий"):
+        print("зеленый")
+    else:
+        print(f"При смешивании {color1} и {color2} получается тот же цвет")
+        
+else:
+    print("Ошибка: введите только основные цвета (красный, синий, желтый)")
